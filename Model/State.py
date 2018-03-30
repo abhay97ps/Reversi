bdd3cb98a9c04578fdc3579696e36894cdbb1607
abhay97ps <abhay97ps@gmail.com>
2018-03-30 13:44:27 +0530

    code reformatting

--- Model/State.py
+++ Model/State.py
@@ -1,14 +1,15 @@
 import numpy as np
 from Bot.Heuristic import evaluate
 
+
 class State:
-    Board = np.zeros((8,8))
+    Board = np.zeros((8, 8))
     Value = 0
     # The two types of pieces
     Black = 1
     Whtie = -1
 
-    def __init__(self,board):
+    def __init__(self, board):
         self.Board = board
         self.Value = self.value()
 
